b111ae1dcb2547f7b892b9e438283a455995c6be
judell <jon@jonudell.info>
2019-04-17 12:03:49 -0700

    try someOverlap and noOverlap options

--- pagefit.js
+++ pagefit.js
@@ -1,6 +1,37 @@
-function adjust(e) {
+function maybeLoadPageFitter() {
+  chrome.storage.sync.get( 
+    {
+      pageFitter: 'default',
+    },
+    function(items) {
+      if (items.pageFitter === 'noOverlap') {
+        function adjuster() {
+          adjust(document.querySelector('body'), false)
+        }
+        window.onload = adjuster
+        window.onresize = adjuster
+      } else if (items.pageFitter === 'someOverlap') {
+        function adjuster() {
+          adjust(document.querySelector('body'), true)
+        }
+        window.onload = adjuster
+        window.onresize = adjuster
+      } else {
+        // retain default behavior
+      }
+    }
+  )
+}
+
+maybeLoadPageFitter()
+
+function adjust(e, someOverlap) {
   e.style.width = '100%'
   const eWidth = parseInt(window.getComputedStyle(e)['width'].replace('px',''))
+  console.log(eWidth)
+  if ( someOverlap && ( eWidth < 1000 || eWidth > 1500) ) {
+    return
+  }
   const bodyWidth = window.innerWidth
   const diff = bodyWidth - eWidth
   const adjustment = 428
@@ -9,13 +40,4 @@ function adjust(e) {
 	e.style.width = adjusted
   }
 }
-
-function adjuster() {
-  adjust(document.querySelector('body'))
-}
-
-
-window.onload = adjuster
-
-window.onresize = adjuster
 
--- src/chrome/lib/options.js
+++ src/chrome/lib/options.js
@@ -1,8 +1,20 @@
 'use strict';
 
 function saveOptions() {
+  const pageFitters = document.getElementsByName('pdfPagefit')
+  alert(pageFitters)
+  let pageFitter
+  for (let i = 0, length = pageFitters.length; i < length; i++) {
+      if (pageFitters[i].checked) {
+          pageFitter = pageFitters[i].value
+          alert(pageFitter)
+          break
+      }
+  }
+
   chrome.storage.sync.set({
     badge: document.getElementById('badge').checked,
+    pageFitter: pageFitter
   });
 }
 
@@ -10,12 +22,29 @@ function loadOptions() {
   chrome.storage.sync.get(
     {
       badge: true,
+      pageFitter: 'default'
     },
     function(items) {
       document.getElementById('badge').checked = items.badge;
+      const pageFitters = document.getElementsByName('pdfPagefit')
+      alert(pageFitters)
+      let pageFitter
+      for (let i = 0, length = pageFitters.length; i < length; i++) {
+          if (pageFitters[i].value === items.pageFitter) {
+              pageFitters[i].checked = true
+              alert(pageFitter)
+              break
+          }
+      }
+    
     }
   );
 }
 
 document.addEventListener('DOMContentLoaded', loadOptions);
 document.getElementById('badge').addEventListener('click', saveOptions);
+const pageFitters = document.getElementsByName('pdfPagefit')
+pageFitters.forEach( pageFitter => {
+  alert(pageFitter)
+  pageFitter.addEventListener('click', saveOptions)
+})
