a87c7d1e6ed3c278402c0ebfe4451646bf488539
Jon Udell <judell@hypothes.is>
2019-04-26 16:42:58 +0000

    add controlled tags option

--- src/chrome/lib/options.js
+++ src/chrome/lib/options.js
@@ -2,7 +2,7 @@
 
 function saveOptions() {
   const pageFitters = document.getElementsByName('pdfPagefit')
-  alert(pageFitters)
+  
   let pageFitter
   for (let i = 0, length = pageFitters.length; i < length; i++) {
       if (pageFitters[i].checked) {
@@ -17,6 +17,21 @@ function saveOptions() {
     pageFitter: pageFitter,
     breakpoint: document.getElementById('pdfOverlapBreakpoint').value
   });
+
+  let tags = document.getElementById('tags').value
+  if (tags) {
+      try {
+      tags = tags.split(',').map(tag => { return ' ' + tag.trim() } )
+      localStorage.setItem('hypothesis.user.tags.list', JSON.stringify(tags))
+      const map = {}
+      tags.forEach(tag => {
+        map[tag] = {}
+      })
+      localStorage.setItem('hypothesis.user.tags.map', JSON.stringify(map))
+    } catch { 
+      console.error(e) 
+    }
+  }
 }
 
 function loadOptions() {
@@ -41,12 +56,21 @@ function loadOptions() {
       }
     
     }
-  );
+  )
+
+  try {
+    let tags = JSON.parse(localStorage.getItem('hypothesis.user.tags.list'))
+    document.getElementById('tags').value =  tags.map(t => { return t.trim() }).join(', ')
+  } catch (e) {
+    console.error(e)
+  }
+
 }
 
 document.addEventListener('DOMContentLoaded', loadOptions)
 document.getElementById('badge').addEventListener('click', saveOptions)
 document.getElementById('pdfOverlapBreakpoint').onchange = saveOptions
+document.getElementById('tags').onchange = saveOptions
 
 const pageFitters = document.getElementsByName('pdfPagefit')
 pageFitters.forEach( pageFitter => {
