43d6a7d450acbd98cf104d96b164428d839c5027
Jon Udell <judell@hypothes.is>
2019-05-08 19:51:52 +0000

    wip

--- htmlPagefit.js
+++ htmlPagefit.js
@@ -9,7 +9,7 @@ window.hypothesisConfig = function() {
       localStorage.setItem('hypothesisLayout', JSON.stringify(obj))
       hypothesisAdjust()
       },
-    "openSidebar": true
+    "openSidebar": false
   }
 }
 
@@ -21,22 +21,8 @@ document.body.innerHTML = hypothesisWrapper.outerHTML
 const hypothesisAllElements = Array.from(document.querySelectorAll('div'))
   .concat( Array.from(document.querySelectorAll('p')) )
   .concat( Array.from(document.querySelectorAll('blockquote')) )
+  .concat( Array.from(document.querySelectorAll('iframe')) )
 		  
-const hypothesisOriginalCss = []
-
-const hypothesisWideElements = hypothesisAllElements.filter ( e => { 
-  return e.clientWidth > hypothesisComputeWrapperWidth()
-})
-  
-for (let i = 0; i < hypothesisWideElements.length; i++) {
-  const e = hypothesisWideElements[i]	
-  hypothesisOriginalCss.push({ 
-    "width": e.clientWidth, 
-    "marginLeft": e.style.marginLeft,
-    "paddingLeft": e.style.paddingLeft
-   })
-}
-
 function hypothesisComputeWrapperWidth() {
   const wrapper = document.getElementById('hypothesisWrapper')
   const layout = JSON.parse(localStorage.getItem('hypothesisLayout'))
@@ -45,20 +31,28 @@ function hypothesisComputeWrapperWidth() {
   return (wrapperWidth - sidebarWidth )
 }
 
-function hypothesisWideElementsAdjust(width) {
-  for (let i = 0; i < hypothesisWideElements.length; i++) {
-    const element = hypothesisWideElements[i]
-    element.style.width = '100%'
-    element.style.width = hypothesisComputeWrapperWidth()
-  }
+function hypothesisWideElementsAdjust() {
+  hypothesisAllElements.forEach ( e => { 
+    const domRect = e.getBoundingClientRect()
+    const width = domRect.x + domRect.width
+    if (width > hypothesisComputeWrapperWidth()) {
+      const viewportOffset = e.getBoundingClientRect().x
+      const adjustedWidth = hypothesisComputeWrapperWidth() - viewportOffset
+      e.style.width = adjustedWidth
+    }
+  })  
 }
 
-function hypothesisAdjust() {
+async function hypothesisAdjust() {
+  function delay(seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000))
+   }
+  await delay(.1) 
   const wrapper = document.getElementById('hypothesisWrapper')
   wrapper.style.width = '100%'
-  const width = `${hypothesisComputeWrapperWidth()}px`
-  hypothesisWideElementsAdjust(width)
-  wrapper.style.width = width
+  wrapper.style.height = '100%'
+  wrapper.style.width = `${hypothesisComputeWrapperWidth()}px`
+  //hypothesisWideElementsAdjust()
 }
 
 //hypothesisWrapperAdjust()
